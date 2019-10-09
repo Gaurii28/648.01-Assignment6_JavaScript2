234630f03d8e7db2b6e85d89e21e81091d608b71
Gaurii28 <gauripatel2803@gmail.com>
2019-10-09 16:24:01 -0700

    part4 update

--- part4/js/inventory-management.js
+++ part4/js/inventory-management.js
@@ -1,7 +1,7 @@
 /*eslint-env browser*/
 var inventory;
 
-function display_Menu(){
+function display_Menu() {
     "use strict";
     window.console.log("Welcome to the Inventory Managemet Application");
     window.console.log("");
@@ -12,38 +12,39 @@ function display_Menu(){
     window.console.log("");
 }
 
-function display_Product(inventory){
-    for (var i =0;i<inventory.length ; i++ ){
-            window.console.log(inventory[i][0]+ " "+inventory[i][1]+ " ("+inventory[i][2]+") $"+inventory[i][3]);
+function display_Product(inventory) {
+    for (var i = 0; i < inventory.length; i++) {
+        window.console.log(inventory[i][0] + " " + inventory[i][1] + " (" + inventory[i][2] + ") $" + inventory[i][3]);
     }
     window.console.log("");
 }
 
-function update(inventory){
+function update(inventory) {
     var number = window.prompt("Enter a sku number!!");
     var stock = window.prompt("Enter a stock quantity!!");
-    for(var i =0;i<=inventory.length;i++){
-        if(inventory[i][0]== number){
-            inventory[i][2]=stock;
+    for (var i = 0; i <= inventory.length; i++) {
+        if (inventory[i][0] === number) {
+            inventory[i][2] = stock;
             break;
         }
     }
 }
 
-function main(){
-    inventory=[[4824,"Shirt",10,15.99],[6343,"Jeans",22,39.99],[3223,"Socks",36,9.99],[2233,"Hat",12,14.99],[9382,"Jacket",5,49.99]];
+function main() {
+    inventory = [[1, "Shirt", 10, 15.99], [2, "Jeans", 22, 39.99], [3, "Socks", 36, 9.99], [4, "Hat", 12, 14.99], [5, "Jacket", 5, 49.99]];
     display_Menu();
-    var command=window.prompt("Enter Command!!!")
-    
-    if(command== "view"){
-        display_Product(inventory);
-    }else if(command== "update"){
-        update(inventory);
-        display_Product(inventory);
-    }else if(command== "exit"){
-        
-    }else {
-        window.alert("Invalid command!!!");
+    while (true) {
+        var command = window.prompt("Enter Command!!!")
+
+        if (command == "view") {
+            display_Product(inventory);
+        } else if (command == "update") {
+            update(inventory);
+        } else if (command == "exit") {
+            break;
+        } else {
+            window.alert("Invalid command!!!");
+        }
     }
 }
 main();
